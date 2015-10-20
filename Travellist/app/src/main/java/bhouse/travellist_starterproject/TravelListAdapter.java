635ac7d2c11addccf7b3a09dee11f23f1f931c02
Priju Jacob Paul <priju.jacobpaul@mintwireless.com>
2015-10-21 10:12:42 +1100

    Material Design
    Material Design that explores RecyclerView

---
+++ Travellist/app/src/main/java/bhouse/travellist_starterproject/TravelListAdapter.java
@@ -0,0 +1,89 @@
+package bhouse.travellist_starterproject;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by priju.jacobpaul on 20/10/2015.
+ */
+public class TravelListAdapter extends RecyclerView.Adapter<TravelListAdapter.ViewHolder>{
+
+    Context mContext;
+    TravelListAdapter.OnItemClickListener mItemClickListener;
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    public TravelListAdapter(Context context) {
+        this.mContext = context;
+
+    }
+
+    @Override
+    public TravelListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_places, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final TravelListAdapter.ViewHolder holder, int position) {
+        final Place place = new PlaceData().placeList().get(position);
+        holder.placeName.setText(place.name);
+        Picasso.with(mContext).load(place.getImageResourceId(mContext)).into(holder.placeImage);
+
+        Bitmap photo = BitmapFactory.decodeResource(mContext.getResources(),place.getImageResourceId(mContext));
+        Palette.generateAsync(photo,new Palette.PaletteAsyncListener(){
+            public void onGenerated(Palette palette) {
+                int bgColor = palette.getMutedColor(mContext.getResources().getColor(android.R.color.black));
+                holder.placeNameHolder.setBackgroundColor(bgColor);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return new PlaceData().placeList().size();
+    }
+
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public LinearLayout placeHolder;
+        public LinearLayout placeNameHolder;
+        public TextView placeName;
+        public ImageView placeImage;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            placeHolder = (LinearLayout) itemView.findViewById(R.id.mainHolder);
+            placeName = (TextView) itemView.findViewById(R.id.placeName);
+            placeNameHolder = (LinearLayout) itemView.findViewById(R.id.placeNameHolder);
+            placeImage = (ImageView) itemView.findViewById(R.id.placeImage);
+            placeHolder.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (mItemClickListener != null) {
+                mItemClickListener.onItemClick(itemView, getPosition());
+            }
+        }
+    }
+
+    public void setOnItemClickListener(final OnItemClickListener mItemClickListener) {
+        this.mItemClickListener = mItemClickListener;
+    }
+}
